97f5c29b90fd571f4fc55cbc8b4882f80d0114f0
sidtuladhar <smt9598@nyu.edu>
2023-06-14 13:41:12 +0545

    created register and login page

---
+++ lib/main_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'ui/login.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFFEA0000),
+        ),
+      ),
+      home: const Login(),
+    );
+  }
+}
