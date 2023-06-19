e3095ffa770b22819019f2ca258693cc71bb7f0f
sidtuladhar <smt9598@nyu.edu>
2023-06-19 12:28:28 +0545

    added home button files

---
+++ lib/ui/home/general.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class General extends StatefulWidget {
+  const General({super.key});
+
+  @override
+  State<General> createState() => _GeneralState();
+}
+
+class _GeneralState extends State<General> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/ui/home/pressure.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Pressure extends StatefulWidget {
+  const Pressure({super.key});
+
+  @override
+  State<Pressure> createState() => _PressureState();
+}
+
+class _PressureState extends State<Pressure> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
