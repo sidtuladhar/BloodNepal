59fae1ad92a2ee6506817d966f1fb7ef88137570
sidtuladhar <smt9598@nyu.edu>
2023-06-19 16:29:07 +0545

    finished home design

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -4,7 +4,7 @@ import 'package:blood_nepal/ui/home/general.dart';
 import 'package:blood_nepal/ui/home/leaderboard.dart';
 import 'package:blood_nepal/ui/home/pressure.dart';
 import 'package:blood_nepal/ui/home/locations.dart';
-import 'package:blood_nepal/ui/signup.dart';
+
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -18,117 +18,142 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: BackButton(color: Colors.red[900]),
-        backgroundColor: Colors.red[900],
-        toolbarHeight: 85,
-        elevation: 10,
-        actions: [
-          Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Siddhartha\nTuladhar",
-                          style: TextStyle(
-                              fontSize: 20, letterSpacing: 1.2, height: 1.2),
-                          textAlign: TextAlign.left),
-                    ],
-                  ),
-                  const SizedBox(width: 35),
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("BLOOD\nTYPE",
-                          style: TextStyle(fontSize: 15),
-                          textAlign: TextAlign.left),
-                      Text("O+",
-                          style: TextStyle(
-                              fontSize: 25, fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.left)
-                    ],
-                  ),
-                  const SizedBox(width: 30),
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("TOTAL\nDONATIONS",
-                          style: TextStyle(fontSize: 15),
-                          textAlign: TextAlign.left),
-                      Text("5",
-                          style: TextStyle(
-                              fontSize: 25, fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.left)
-                    ],
-                  ),
-                  const SizedBox(width: 15),
-                  IconButton(
-                    onPressed: () {
-                      _boxLogin.clear();
-                      _boxLogin.put("loginStatus", false);
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return const Login();
-                          },
-                        ),
-                      );
-                    },
-                    icon: const Icon(Icons.logout_rounded),
-                  ),
-                ],
-              )),
-        ],
-      ),
-      backgroundColor: Colors.blueGrey[100],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(height: 50),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  homeButton(
-                      "General Info", context, const General(), Icons.info),
-                  homeButton("Donation History", context, const History(),
-                      Icons.article),
-                ]),
-            const SizedBox(height: 50),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  homeButton("Leaderboard", context, const Leaderboard(),
-                      Icons.leaderboard),
-                  homeButton("Blood Pressure", context, const Pressure(),
-                      Icons.health_and_safety),
-                ]),
-            const SizedBox(height: 50),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  homeButton("Blood Bank Locations", context, const Locations(),
-                      Icons.location_on),
-                  homeButton(
-                      "Settings", context, const Settings(), Icons.settings),
-                ])
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          shape: ShapeBorder.lerp(
+              RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: const BorderSide(color: Colors.red)),
+              RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: const BorderSide(color: Colors.red)),
+              10),
+          leading: BackButton(color: Colors.red[900]),
+          backgroundColor: Colors.red[900],
+          toolbarHeight: 85,
+          //elevation: 10,
+          actions: [
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Siddhartha\nTuladhar",
+                            style: TextStyle(
+                                fontSize: 20, letterSpacing: 1.2, height: 1.2),
+                            textAlign: TextAlign.left),
+                      ],
+                    ),
+                    const SizedBox(width: 35),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("BLOOD\nTYPE",
+                            style: TextStyle(fontSize: 15),
+                            textAlign: TextAlign.left),
+                        Text("O+",
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.left)
+                      ],
+                    ),
+                    const SizedBox(width: 30),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("TOTAL\nDONATIONS",
+                            style: TextStyle(fontSize: 15),
+                            textAlign: TextAlign.left),
+                        Text("5",
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.left)
+                      ],
+                    ),
+                    const SizedBox(width: 15),
+                    IconButton(
+                      onPressed: () {
+                        _boxLogin.clear();
+                        _boxLogin.put("loginStatus", false);
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return const Login();
+                            },
+                          ),
+                        );
+                      },
+                      icon: const Icon(Icons.logout_rounded),
+                    ),
+                  ],
+                )),
           ],
         ),
-      ),
-    );
+        body: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(height: 20),
+                const Text(
+                  'Appointment',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                getAppointment(),
+                const SizedBox(height: 20),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      homeButton("Blood Bank Locations", context,
+                          const Locations(), Icons.location_on),
+                      homeButton("Donation History", context, const History(),
+                          Icons.article),
+                    ]),
+                const SizedBox(height: 20),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      homeButton("Leaderboard", context, const Leaderboard(),
+                          Icons.leaderboard),
+                      homeButton("Blood Pressure", context, const Pressure(),
+                          Icons.health_and_safety),
+                    ]),
+                const SizedBox(height: 20),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      homeButton(
+                          "General Info", context, const General(), Icons.info),
+                      homeButton("Settings", context, const Settings(),
+                          Icons.settings),
+                    ])
+              ],
+            ),
+          ),
+          const SizedBox(height: 50),
+          Text("Version 0.5",
+              style: TextStyle(
+                  fontSize: 15,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.grey[900])),
+        ])));
   }
 }
 
 homeButton(String text, context, target, IconData icon) {
   return ElevatedButton(
       style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.red[900],
-        elevation: 10,
-        fixedSize: const Size.fromRadius(75),
+        backgroundColor: Colors.red[700],
+        elevation: 5,
+        fixedSize: const Size(170, 110),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
@@ -146,8 +171,8 @@ homeButton(String text, context, target, IconData icon) {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(icon, color: Colors.white, size: 50),
-          const SizedBox(height: 10),
+          Icon(icon, color: Colors.white, size: 45),
+          const SizedBox(height: 5),
           Text(
             text,
             style: const TextStyle(fontSize: 20),
@@ -155,4 +180,18 @@ homeButton(String text, context, target, IconData icon) {
           ),
         ],
       ));
+}
+
+Widget getAppointment() {
+  return Container(
+    width: double.infinity,
+    height: 150,
+    decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.grey, width: .1)),
+    padding: const EdgeInsets.all(18),
+    child: const Text("You currently have no appointment.",
+        style: TextStyle(fontSize: 20)),
+  );
 }
